3840752a074fea48e990f967a275d8e251bb4300
itscatherinelee <itscatherinehlee@gmail.com>
2024-11-08 16:05:30 -0500

    Add confeetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useRef } from "react";
 import { BirthdayCountdown } from "./components/Countdown";
+import party from 'party-js';
 
 function App() {
+    const isConfettiOn = useRef(false);
+
+    useEffect(() => {
+      const shootConfetti = (event: MouseEvent) => {
+        if (!isConfettiOn.current) {
+          isConfettiOn.current = true;
+  
+          party.confetti(event, {
+            count: party.variation.range(0, 5),
+            spread: 100,
+          });
+  
+          requestAnimationFrame(() => {
+            isConfettiOn.current = false;
+          });
+        }
+      };
+  
+      document.addEventListener('mousemove', shootConfetti);
+      return () => {
+        document.removeEventListener('mousemove', shootConfetti);
+      };
+    }, []);
 
   return (
     <div>
