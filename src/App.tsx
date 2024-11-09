2e1d40d767feaeb05b6dcc88b726906bd5fcb62b
Shams Jamal <shams.jamal67@gmail.com>
2024-11-08 21:52:07 -0500

    dvd richard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { BirthdayCountdown } from "./components/Countdown";
+import { DvdRichard } from "./components/DvdRichard";
 
 function App() {
 
   return (
     <div>
       <BirthdayCountdown />
+      <div style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: -2,
+      }}>
+        <DvdRichard size={250} />
+      </div>
     </div>
   );
 }
