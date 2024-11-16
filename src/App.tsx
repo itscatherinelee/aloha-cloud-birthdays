de6c404a1b9e52f568eba4a9a97dce87e3421bef
itscatherinelee <itscatherinehlee@gmail.com>
2024-11-16 14:20:25 -0500

    refactor richard's supplement

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useEffect, useRef } from "react";
 import { CountdownRichard } from "./components/richard/CountdownRichard";
-import { DvdRichard } from "./components/richard/DvdRichard";
 import "./App.css";
 import Confetti from "./components/Confetti";
 import { Link } from "react-router-dom";
-import { RollingAlert } from "./components/RollingAlert";
 
 function App() {
   const isConfettiOn = useRef(false);
@@ -16,10 +14,6 @@ function App() {
     <div>
       <Link to="/upcoming">Who's Next????!</Link>
       <CountdownRichard />
-      <div className="dvd-container">
-        <DvdRichard size={250} />
-      </div>
-      <RollingAlert />
     </div>
   );
 }
--- src/components/richard/CountdownRichard.tsx
+++ src/components/richard/CountdownRichard.tsx
@@ -2,32 +2,40 @@ import Richard from "../../assets/richard/richard.png";
 import "./CountdownRichard.css";
 import Countdown from "react-countdown";
 import CelebrationRichard from "./CelebrationRichard";
+import { DvdRichard } from "./DvdRichard";
+import { RollingAlert } from "./RollingAlertRichard";
 
 export function CountdownRichard() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "50px",
-      }}
-    >
-      <img className="image" src={Richard} />
-      <Countdown
-        date={new Date("12-19-2024").setFullYear(new Date().getFullYear())}
-        renderer={({ days, hours, minutes, seconds, completed }) => {
-          if (completed) {
-            return <CelebrationRichard />;
-          }
-          return (
-            <h1>
-              {days} days {hours} hours {minutes} minutes {seconds} seconds
-            </h1>
-          );
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px",
         }}
-      ></Countdown>
-      <img className="image" src={Richard} />
-    </div>
+      >
+        <img className="image" src={Richard} />
+        <Countdown
+          date={new Date("12-19-2024").setFullYear(new Date().getFullYear())}
+          renderer={({ days, hours, minutes, seconds, completed }) => {
+            if (completed) {
+              return <CelebrationRichard />;
+            }
+            return (
+              <h1>
+                {days} days {hours} hours {minutes} minutes {seconds} seconds
+              </h1>
+            );
+          }}
+        ></Countdown>
+        <img className="image" src={Richard} />
+      </div>
+      <div className="dvd-container">
+        <DvdRichard size={250} />
+      </div>
+      <RollingAlert />
+    </>
   );
 }
